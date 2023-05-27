3b21b8f4265b61cf9aaf9e8d32e1a50bc82e2a40
brandon <brandonnlee2210@gmail.com>
2023-05-27 10:39:10 +0700

    fix notification page

--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -1,7 +1,7 @@
 import Header from "@/components/Header";
 import NotificationsFeed from "@/components/NotificationsFeed";
 
-export async function Page() {
+export default async function Home() {
   return (
     <div>
       <Header label="Notifications" showBackArrow />
@@ -9,4 +9,3 @@ export async function Page() {
     </div>
   );
 }
-export default Page;
